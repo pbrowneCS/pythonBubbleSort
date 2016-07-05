f4846e7ac03cddafa6e22a675d39997689c49d90
Peter Browne <PeterMatthewBrowne@gmail.com>
2016-07-05 15:35:59 -0700

    first commit

---
+++ bubbleSort001.py
@@ -0,0 +1,23 @@
+import random
+arr = []
+for i in range(100):
+	arr.append(random.randint(0,10000))
+
+def bubbleSorting(x):
+	larger = arr[0]
+	for i in range(0,len(x)):
+		for j in range(1,len(x)-(i)):
+			if x[j] < x[j-1]:
+				x[j-1], x[j] = x[j], x[j-1]
+	print x
+bubbleSorting(arr)
+
+
+#take array
+#check first against next number
+#if first number, swap first with next
+#check next number
+#repeat check
+#i = 0>1
+#j = 1>2>3
+#arr = [6,9,5,7,4,2,3,1]
